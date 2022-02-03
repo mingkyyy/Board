9f426a0c96fcff6d1b7b11c6f12da3dcc773d4c1
mingkyy <alsrus1227@naver.com>
2022-02-03 17:13:25 +0900

    로그인 테스트 코드 작성

--- src/test/java/com/mingky/Board/controller/MemberControllerTest.java
+++ src/test/java/com/mingky/Board/controller/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.mingky.Board.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mingky.Board.domain.Member;
 import com.mingky.Board.domain.MemberType;
 import com.mingky.Board.dto.SignupDto;
@@ -20,7 +21,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
@@ -28,6 +33,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -49,10 +55,10 @@ public class MemberControllerTest {
 
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebApplicationContext context;
 
     @Autowired
-    private MemberService memberService;
+    private MockMvc mockMvc;
 
     @AfterEach
     public void cleanMember() throws Exception{
@@ -89,21 +95,35 @@ public class MemberControllerTest {
 
 
 
+    @BeforeEach
+    public void setup(){
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.context)
+                .apply(springSecurity())
+                .build();
+        Member member = Member.builder()
+                .name("mingkyy")
+                .email("test@test.com")
+                .nickname("mi")
+                .password(passwordEncoder.encode("12345"))
+                .memberType(MemberType.ROLE_JUN)
+                .build();
+
+        memberRepository.save(member);
+    }
+
+
     @Test
     public void 로그인_테스트() throws Exception{
-        String name = "test";
-        String username = "test@naver.com";
+        String username = "test@test.com";
         String password = "12345";
-        String nickname = "mingkyy";
 
-        SignupDto signupDto = new SignupDto(username, name, password, nickname);
-        memberService.save(signupDto);
-
-        mockMvc.perform(post("/login")
-                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-                .content("username=test@naver.com&password=12345"))
-                .andExpect(status().isFound())
-                .andExpect(header().stringValues(HttpHeaders.LOCATION, "/"));
+        mockMvc.perform(formLogin()
+                        .user(username)
+                        .password(password))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
 
     }
 
