7ffaf8c211f844f9536e292d16a1f9428cefd952
mingkyy <alsrus1227@naver.com>
2022-02-04 20:41:38 +0900

    프론트 화면 구성

--- src/main/resources/static/js.member/signup.js
+++ src/main/resources/static/js.member/signup.js
@@ -1,5 +1,10 @@
 $(function () {
     $('#signup').click(function () {
+        if ($('#hidden').val() == "aa"){
+            alert("핸드폰 인증에 성공하셔야 합니다.");
+            return false;
+        }
+
         if ($('#name').val()==''){
             alert("이름을 입력하세요");
             $('#name').focus();
@@ -53,7 +58,8 @@ $(function () {
                 name: $('#name').val(),
                 nickname: $('#nickname').val(),
                 password: $('#password').val(),
-                email: $('#email').val()
+                email: $('#email').val(),
+                tel : $('#tel').val()
             };
 
             $.ajax({
@@ -69,4 +75,32 @@ $(function () {
                 alert(JSON.stringify(error));
             });
         });
+
+    $('#sendPhoneNumber').click(function(){
+        let phoneNumber = $('#tel').val();
+        if (phoneNumber == null){
+            alert("핸드폰 번호를 입력해주세요")
+            return;
+        }
+        alert("인증번호가 전송되었습니다");
+        $.ajax({
+            type: "GET",
+            url: "/check/sendSMS",
+            data: {
+                "phoneNumber" : phoneNumber
+            },
+            success: function(res){
+                $('#checkBtn').click(function(){
+                    if($.trim(res) ==$('#inputCertifiedNumber').val()){
+                        $('#hidden').val("zzz");
+                        alert("핸드폰 인증에 성공하였습니다.");
+                    }else{
+                        alert("인증번호가 다릅니다.");
+                    }
+                })
+
+
+            }
+        })
+    });
 });
